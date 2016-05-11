270eb8a78a0b2508c79ba9eb8d229666fb719aa9
Brent Gordon <brentgordon01@gmail.com>
2016-05-11 12:13:51 +0200

    Initial commit changes

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*globals JSON, PouchDB, Promise cordova*/
+
+/** 
+ * Module description
+ *
+ * @module
+ * @param {string} param1 - Description
+ * @param {Object} [options]
+ * 	@param {string} options.param1 
+ *	Description
+ * @author Brent Gordon
+ * @version 0.1.0
+ *
+ * @example 
+ * 
+ *
+ * @return {Object} Type
+ *
+ * @throws "ERROR: Message"
+ *
+ */
+
+ 
