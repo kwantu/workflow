1a4671e7f66d0bf99b9112e012e61947e54c2e5c
Brent Gordon <brentgordon01@gmail.com>
2016-06-17 15:18:27 +0200

    Added better error handling and cleaned up the code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var Process = require('./lib/process');
 
 var util = require('./lib/utility');
-var models = require('./lib/models');
 
 /*globals */
 
@@ -72,31 +71,118 @@ function Workflow(profile, app, config, instance){
  *
  */
 Workflow.prototype.create = function(){
-	//
+	// Re-assign the Workflow constructor instance as _this
 	var _this = this;
-	// Use the native es6 Promise constructor
 	return new Promise(function(resolve, reject) {
-		if (_this.instance !== undefined) {
-			var warn = util.warn('Instance already exists.', _this.instance)
-			resolve(warn);
-		} else {
-			// Create the workflow processes instance object
-			var model = models.instance();
-			model._id = _this.profile + ':processes';
-			model.version = _this.config.version;
-			// Update the processes and subProcesses sections with defaults
-			// model.processes[0].id = _this.config.processes[0]._id;
-			// model.processes[0].seq = 1;
-			_this.instance = model;
-			var success = util.success('Workflow processes instance created successfully.', _this.instance);
-			resolve(success);
+		// Try the block of code
+		try {
+			if (_this.instance !== undefined) {
+				var warn = util.warn('Instance already exists.', _this.instance)
+				resolve(warn);
+			} else {
+				// Create the workflow processes instance object
+				var model = {
+				    _id: "",
+				    version: "",
+				    type: "workflowInstance",
+				    processes: []
+				};
+				model._id = _this.profile + ':processes';
+				model.version = _this.config.version;
+				_this.instance = model;
+				var success = util.success('Workflow processes instance created successfully.', _this.instance);
+				resolve(success);
+			}
+		// Catch any unforseen errors
+		} catch (err) {
+			reject(err);
 		}
 	});
 };
 
 /** 
- * Workflow initialize, this function executes a process within a workflow
+ * Workflow initialise, this function executes a process within a workflow
  * configuration.
+ *
+ * @param {string} processId - the process id to process
+ * @param {object} data - the input data to process
+ *
+ * @example 
+ * Workflow.initialize('processId', { validDate: 'date' });
+ *
+ * @return 
+ * { complete: true, message: 'Process: 'processId' initialized successfully.', res: undefined }
+ *
+ */
+Workflow.prototype.initialise = function(processId, data){
+	// Re-assign the Workflow constructor instance as _this
+	var _this = this;
+	return new Promise(function(resolve, reject) {
+		try {
+			var configProcess = [];
+			// Check the passed in parameters
+			if (processId !== '' && processId !== undefined) {
+				// Get the current process config
+				configProcess = _this.config.processes.filter(function(objProcess){
+					if (objProcess._id === processId) {
+						return objProcess;
+					}
+				});
+				if (configProcess[0]._id === undefined) {
+					var error = util.error('WFConfigError', 'No valid process definition found with process id: ' + processId);
+					reject(error);
+				}
+			} else {
+				configProcess.push(_this.config.processes[0]);
+				processId = _this.config.processes[0]._id;
+			}
+			// Get the current list of process instances
+			var processSeq = 1;
+			var currentProcess = [];
+			currentProcess = _this.instance.processes.filter(function(objProcess){
+				if (objProcess.id === processId) {
+					return objProcess;
+				}
+			});
+			var currentSeq = currentProcess.length;
+			var nextSeq = currentSeq + 1;
+			// Push the process object into the array
+			var processModel = {
+				"id": "", 
+		      	"seq": "", 
+		      	"subProcesses": []
+			}
+			// 1. Update the process id and seq
+			processModel.id = processId;
+			processModel.seq = nextSeq;
+			_this.instance.processes.push(processModel);
+			// 2. Complete all the process prerequisites
+			Process.preRequisites(configProcess[0].prerequisites, _this).then(function(result){
+				// 3. Complete all the process pre-actions
+				Process.preActions(configProcess[0].preActions, _this).then(function(result){
+					// 4. Initialise the first sub-process
+					Process.subProcess(processId, configProcess[0].subProcesses[0], 1, data, _this).then(function(result){
+						// console.log('subProcess completed.');
+						_this.instance = result.res;
+						var success = util.success('Process: ' + _this.config.processes[0]._id + ' initialized successfully.');
+						resolve(success);
+					}, function(err){
+						reject(err);
+					});
+				}, function(err){
+					reject(err);
+				});
+			}, function(err){
+				reject(err);
+			});
+		} catch(err) {
+			reject(err);
+		}
+	});
+};
+
+/** 
+ * Workflow task, this method executes a specific task.
  *
  * @param {string} processId - the process id to process
  * @param {object} inputData - the input data to process
@@ -108,135 +194,50 @@ Workflow.prototype.create = function(){
  * { complete: true, message: 'Process: 'processId' initialized successfully.', res: undefined }
  *
  */
-Workflow.prototype.initialize = function(processId, inputData){
-	// Re-assign this 
-	var _this = this;
-	// Use the native es6 Promise constructor
-	return new Promise(function(resolve, reject) {
-		var configProcess = [];
-		// Check the passed in parameters
-		if (processId !== '' && processId !== undefined) {
-			// Get the current process config
-			configProcess = _this.config.processes.filter(function(objProcess){
-				if (objProcess._id === processId) {
-					return objProcess;
-				}
-			});
-			if (configProcess[0]._id === undefined) {
-				var error = util.error('WF001', err);
-				reject(error);
-			}
-		} else {
-			configProcess.push(_this.config.processes[0]);
-			processId = _this.config.processes[0]._id;
-		}
-		// Get the current list of process instances
-		var processSeq = 1;
-		var currentProcess = [];
-		currentProcess = _this.instance.processes.filter(function(objProcess){
-			if (objProcess.id === processId) {
-				return objProcess;
-			}
-		});
-		var currentSeq = currentProcess.length;
-		var nextSeq = currentSeq + 1;
-		// Push the process object into the array
-		var processModel = {
-			"id": "", 
-	      	"seq": nextSeq, 
-	      	"subProcesses": []
-		}
-		// 1. Update the process id and seq
-		processModel.id = processId;
-		processModel.seq = processSeq;
-		_this.instance.processes.push(processModel);
-		// 2. Complete all the process prerequisites
-		Process.preRequisites(configProcess[0].prerequisites, _this).then(function(result){
-			// Check if all pre-requisites were met
-			if (result.complete === true) {
-				// 3. Complete all the process pre-actions
-				Process.preActions(configProcess[0].preActions, _this).then(function(result){
-					// Check if all pre-actions were met
-					if (result.complete) {
-						// 4. Initialise the first sub-process
-						Process.subProcess(processId, configProcess[0].subProcesses[0], 1, inputData, _this).then(function(result){
-							// Check if sub-process initialisation was successfull
-							if (result.complete) {
-								// 5. Update the subProcess section details in the processes model
-								var subProcessModel = result.res;
-								// _this.instance.processes[processIndex].subProcesses.push(subProcessModel);
-								_this.instance.processes.filter(function(objProcess){
-									if (objProcess.id === processId) {
-										objProcess.subProcesses.push(subProcessModel);
-									}
-								});
-								var success = util.success('Process: ' + _this.config.processes[0]._id + ' initialized successfully.');
-								resolve(success);
-							} else {
-								var error = util.error('WF008');
-								reject(error);
-							}
-						}, function(err){
-							reject(err);
-						});
-					} else {
-						var error = util.error('WF006');
-						reject(error);
-					}
-				}, function(err){
-					reject(err);
-				});
-			} else {
-				var error = util.error('WF004');
-				reject(error);
-			}
-		}, function(err){
-			reject(err);
-		});
-	});
-};
-
 Workflow.prototype.task = function(type, params){
-	// Re-assign this 
+	// Re-assign the Workflow constructor instance as _this
 	var _this = this;
 	return new Promise(function(resolve, reject) {
-		if (type === 'indicator.markComplete') {
+		try {
 			resolve('Success');
-		} else {
-			reject('Error');
+		} catch(err) {	
+			reject(err);
 		}
 	});
 };
 
-Workflow.prototype.transition = function(processId, subProcessId, stepId, transitionId, subProcessModel, workflow){
+/** 
+ * Workflow transition to the next step. This moves the workflow from the current process,
+ * sub-process step to the next one as specified.
+ *
+ * @param {string} processId - the Workflow config / definition process id
+ * @param {string} subProcessId - the Workflow config / definition sub-process id
+ * @param {string} stepId - the Workflow config / definition step id
+ * @param {string} transitionId - the Workflow config / definition transition id
+ * @param {object} data - any additional data passed in as key value pairs
+ *
+ * @example 
+ * Workflow.transition('processId', 'subProcessId', 'stepId', 'transitionId', { key: '', value: '' });
+ *
+ * @return 
+ * { complete: true, message: 'Workflow transitioned to the next step successfully.', res: {} }
+ *
+ */
+Workflow.prototype.transition = function(processId, subProcessId, stepId, transitionId, data){
 	// Re-assign this 
 	var _this = this;
-	// Use the native es6 Promise constructor
 	return new Promise(function(resolve, reject) {
-		Process.transition(processId, subProcessId, stepId, transitionId, subProcessModel, workflow).then(function(result){
-			var success = util.success('Workflow transitioned to the next step successfully.', subProcessModel);
-			resolve(success);
-		}, function(err){
+		try {
+			Process.transition(processId, subProcessId, stepId, transitionId, data, _this).then(function(result){
+				var success = util.success('Workflow transitioned to the next step successfully.', {});
+				resolve(success);
+			}, function(err){
+				reject(err);
+			});
+		} catch(err) {	
 			reject(err);
-		});	
+		}
 	});
 };
 
 module.exports = Workflow;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
